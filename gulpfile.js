3fcdbd81da05c6f548c084a66d8bbbf40bac9e01
denis010601 <57405778+denis010601@users.noreply.github.com>
2020-05-05 13:41:13 +0400

    created a plugin to minimize css files

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
+const cleanCSS = require('gulp-clean-css');
+
+const rename = require("gulp-rename");
 
 gulp.task('hello', function(done){
   console.log('Привет, мир!');
@@ -14,3 +17,11 @@ gulp.task('browser-sync', function() {
   });
   gulp.watch("./*.html").on('change', browserSync.reload);
 });
+
+gulp.task('mincss', function() {
+
+  return gulp.src("./css/*.css")
+  .pipe(rename({suffix: ".min"}))
+  .pipe(cleanCSS())
+  .pipe(gulp.dest("css"));
+})
