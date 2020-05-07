157bd40263d11910805b87135ba6cb1de7db8cd8
denis010601 <57405778+denis010601@users.noreply.github.com>
2020-05-08 00:56:27 +0400

    feat: hero fonts

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,8 @@ const {src, dest, watch} = require('gulp');
 const browserSync = require('browser-sync').create();
 const cleanCSS = require('gulp-clean-css');
 const sass = require('gulp-sass');
-
 const rename = require("gulp-rename");
-
+const autoprefixer = require('gulp-autoprefixer');
 
 function bs() {
   serveSass();
@@ -29,6 +28,9 @@ function mincss() {
 function serveSass() {
   return src("./scss/*.scss")
       .pipe(sass())
+      .pipe(autoprefixer({
+        cascade: false
+        }))
       .pipe(dest("./css"))
       .pipe(browserSync.stream());
 };
