e9142beb8948ff4e4a5fada2674817cb312d77e0
denis010601 <57405778+denis010601@users.noreply.github.com>
2020-05-16 15:45:41 +0400

    feat:     if (.scrollTop() >= 70) {

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
+/*document.addEventListener("DOMContentLoaded", function(event) { 
   const modal = document.querySelector('.modal');
-  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+  const modalBtn = document.querySelectorAll('[data-toggle="modal"]');
   const closeBtn = document.querySelector('.modal__close');
 
   const switchModal = () => {
@@ -16,4 +16,44 @@ document.addEventListener("DOMContentLoaded", function(event) {
       modal.classList.toggle('modal--visible');
     }
   }
+});*/
+
+function backToTop() {
+  let button = $('.back-to-top');
+
+  $(window).on('scroll', () => {
+    if ($(this).scrollTop() >= 70) {
+      button.fadeIn();
+    } else {
+      button.fadeOut();
+    }
+  });
+
+  button.on ('click', (e) => {
+    e.preventDefault ();
+    $('html').animate({scrollTop: 0}, 1000);
+  });
+}
+
+backToTop();
+
+
+
+$(document).ready(function (){
+  var modal = $('.modal'),
+      modalBtn = $('[data-toggle="modal"]'),
+      closeBtn = $('.modal__close');
+      
+  $(document).on('keydown', function(e) {
+    if (e.keyCode == 27)
+    modal.toggleClass('modal--visible');
+    });
+  modalBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
+  });
+  closeBtn.on('click', function (){
+    modal.toggleClass('modal--visible');
+  });
 });
+
+
