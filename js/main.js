412ce412ffc15e7dffce1a6b974ae2ee08d3446d
denis010601 <57405778+denis010601@users.noreply.github.com>
2020-05-22 17:52:01 +0400

    feat new templases teams v2

--- js/main.js
+++ js/main.js
@@ -127,6 +127,68 @@ $(document).ready(function (){
     
     });
     $('[type=tel]').mask('+7(000) 000-00-00', {placeholder: "+7(___) __-__-__"});
+    $(".footer__form").validate({
+      errorElement: "div",
+      errorClass: "invalid",
+      rules: {
+        // simple rule, converted to {required:true}
+        userName: {
+          required: true,
+          minlength: 2
+        },
+        userPhone: "required",
+        // compound rule
+        userEmail: {
+          required: true,
+          email: true
+        }
+      },
+        messages: {
+          userName: {
+            required:"Имя обязательно",
+            minlength: "Имя не короче двух букв"
+
+          } ,
+          userPhone: "Телефон обязателен",
+          userEmail: {
+        required: "Укажите email",
+        email: "введите формате: name@domain.com"
+      }
+    }
+    
+    });
+    $('[type=tel]').mask('+7(000) 000-00-00', {placeholder: "+7(___) __-__-__"});
+    $(".control__form").validate({
+      errorElement: "div",
+      errorClass: "invalid",
+      rules: {
+        // simple rule, converted to {required:true}
+        userName: {
+          required: true,
+          minlength: 2
+        },
+        userPhone: "required",
+        // compound rule
+        userEmail: {
+          required: true,
+          email: true
+        }
+      },
+        messages: {
+          userName: {
+            required:"Имя обязательно",
+            minlength: "Имя не короче двух букв"
+
+          } ,
+          userPhone: "Телефон обязателен",
+          userEmail: {
+        required: "Укажите email",
+        email: "введите формате: name@domain.com"
+      }
+    }
+    
+    });
+    $('[type=tel]').mask('+7(000) 000-00-00', {placeholder: "+7(___) __-__-__"});
 });
 
 
