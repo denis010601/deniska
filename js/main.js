af9f95a4c49312d4b775e4b6bf56d785b6a65ae7
denis010601 <57405778+denis010601@users.noreply.github.com>
2020-05-24 20:26:35 +0400

    reset 24 v3

--- js/main.js
+++ js/main.js
@@ -228,7 +228,7 @@ $(document).ready(function (){
               // Необходимо указать данный тип макета.
               iconLayout: 'default#image',
               // Своё изображение иконки метки.
-              iconImageHref: '../img/location.png',
+              iconImageHref: './img/location.png',
               // Размеры метки.
               iconImageSize: [32, 32],
               // Смещение левого верхнего угла иконки относительно
