6082281875aab227afe04445d8d83d21565f68bd
denis010601 <57405778+denis010601@users.noreply.github.com>
2020-05-28 11:53:03 +0400

    final

--- js/main.js
+++ js/main.js
@@ -1,30 +1,9 @@
-/*document.addEventListener("DOMContentLoaded", function(event) { 
-  const modal = document.querySelector('.modal');
-  const modalBtn = document.querySelectorAll('[data-toggle="modal"]');
-  const closeBtn = document.querySelector('.modal__close');
-
-  const switchModal = () => {
-    modal.classList.toggle('modal--visible');
-  }
-  modalBtn.forEach(element => {
-    element.addEventListener('click', switchModal);
-  });
-  closeBtn.addEventListener('click', switchModal);
-  
-  window.onclick = function (event) {
-    if (event.target == modal) {
-      modal.classList.toggle('modal--visible');
-    }
-  }
-});*/
-
-
-
 
 $(document).ready(function (){
   var modal = $('.modal'),
       modalBtn = $('[data-toggle="modal"]'),
       closeBtn = $('.modal__close');
+      thank = $('.thank')
       
   $(document).on('keydown', function(e) {
     if (e.keyCode == 27)
@@ -35,6 +14,9 @@ $(document).ready(function (){
   });
   closeBtn.on('click', function (){
     modal.toggleClass('modal--visible');
+  });
+  thank.on('click', function () {
+    thank.toggleClass('thank--visible');
   });
   
   //initialize swiper when document ready
@@ -58,29 +40,7 @@ $(document).ready(function (){
   next.css ('left', prev.width() + 20 + bullets.width() + 20)
   bullets.css('left', prev.width() + 20);
 
-    //initialize swiper when document ready
-    var mySwiper = new Swiper ('.step .swiper-container', {
     
-      
-      loop: true,
-      pagination: {
-          el: '.step .swiper-pagination',
-          type: 'bullets',
-          
-      },
-      navigation: {
-        nextEl: '.step .swiper-button-next',
-        prevEl: '.step .swiper-button-prev',
-      },
-    })
-  
-    var next = $('.step .swiper-button-next');
-    var prev = $('.step .swiper-button-prev');
-    var bullets = $('.step .swiper-pagination');
-  
-  
-    next.css ('left', prev.width() + 20 + bullets.width() + 20)
-    bullets.css('left', prev.width() + 20)
 
     new WOW().init();
 
@@ -113,10 +73,12 @@ $(document).ready(function (){
         },
         checkbox: {
           required : true
-        }
+        } 
         
       },
         messages: {
+          
+          checkbox: "Примите соглашение",
           userName: {
             required:"Имя обязательно",
             minlength: "Имя не короче двух букв"
@@ -127,10 +89,20 @@ $(document).ready(function (){
             required: "Укажите email",
             email: "введите формате: name@domain.com"
           },
-         checkbox: "Примите соглашение"
-
-
-    }
+        
+      },
+      submitHandler: function(form) {
+        $.ajax ({
+          type: "POST",
+          url: "send.php",
+          data: $(form).serialize(),
+          success: function (response) {
+            thank.toggleClass('thank--visible');
+            $(form)[0].reset();
+            modal.removeClass('modal--visible');
+          }
+        });
+      }
     });
     $('[type=tel]').mask('+7(000) 000-00-00', {placeholder: "+7(___) __-__-__"});
     
@@ -165,10 +137,22 @@ $(document).ready(function (){
             required: "Укажите email",
             email: "введите формате: name@domain.com"
           },
-         checkbox: "Примите соглашение"
-
-
+        checkbox: "Примите соглашение"
+    },
+    submitHandler: function(form) {
+      $.ajax ({
+        type: "POST",
+        url: "send.php",
+        data: $(form).serialize(),
+        success: function (response) {
+          thank.toggleClass('thank--visible');
+          $(form)[0].reset();
+          modal.removeClass('modal--visible');
+          
+        }
+      });
     }
+
     });
 
     $(".footer__form").validate({
@@ -182,10 +166,7 @@ $(document).ready(function (){
         },
         userPhone: "required",
         // compound rule
-        userquestion: {
-          required: true,
-          email: true
-        },
+        userQuestion: "required",
         checkbox: {
           required : true
         }
@@ -198,44 +179,157 @@ $(document).ready(function (){
 
           } ,
           userPhone: "Телефон обязателен",
-          userquestion: "Задайте вопрос",
+          userQuestion: "Задайте вопрос",
         checkbox: "Примите соглашение"
-
-
+    },
+    submitHandler: function(form) {
+      $.ajax ({
+        type: "POST",
+        url: "send.php",
+        data: $(form).serialize(),
+        success: function (response) {
+          thank.toggleClass('thank--visible');
+          $(form)[0].reset();
+          modal.removeClass('modal--visible');
+        }
+      });
     }
     });
-    ymaps.ready(function () {
-      var myMap = new ymaps.Map('map', {
-              center: [47.244729, 39.723187],
-              zoom: 9
-          }, {
-              searchControlProvider: 'yandex#search'
-          }),
+    //Переменная для включения/отключения индикатора загрузки
+var spinner = $('.ymap-container').children('.loader');
+//Переменная для определения была ли хоть раз загружена Яндекс.Карта (чтобы избежать повторной загрузки при наведении)
+var check_if_load = false;
+//Необходимые переменные для того, чтобы задать координаты на Яндекс.Карте
+var myMapTemp, myPlacemarkTemp;
+ 
+//Функция создания карты сайта и затем вставки ее в блок с идентификатором &#34;map-yandex&#34;
+function init () {
+  var myMapTemp = new ymaps.Map("map-yandex", {
+    center: [47.244729, 39.723187], // координаты центра на карте
+    zoom: 9, // коэффициент приближения карты
+    controls: ['zoomControl', 'fullscreenControl'] // выбираем только те функции, которые необходимы при использовании
+  },
   
-          // Создаём макет содержимого.
-          MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
-              '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
-          ),
+  );
+  myMapTemp.behaviors.disable('drag');
+  myMapTemp.behaviors.disable('scrollZoom')
   
-          myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-              hintContent: 'Наш офис',
-              balloonContent: 'Цокальный этаж'
-          }, {
-              // Опции.
-              // Необходимо указать данный тип макета.
-              iconLayout: 'default#image',
-              // Своё изображение иконки метки.
-              iconImageHref: './img/location.png',
-              // Размеры метки.
-              iconImageSize: [32, 32],
-              // Смещение левого верхнего угла иконки относительно
-              // её "ножки" (точки привязки).
-              iconImageOffset: [-5, -38]
-          });
   
-      myMap.geoObjects
-          .add(myPlacemark);
+  var myPlacemarkTemp = new ymaps.Placemark([47.244729, 39.723187], {
+      balloonContent: "Мы тут",
+  }, 
+  
+  
+  {
+      // Опции.
+      // Необходимо указать данный тип макета.
+      iconLayout: 'default#imageWithContent',
+      // Своё изображение иконки метки.
+      iconImageHref: 'img/map-marker.png',
+      // Размеры метки.
+      iconImageSize: [32, 32],
+      // Смещение левого верхнего угла иконки относительно
+      // её "ножки" (точки привязки).
+      iconImageOffset: [-20, -30],
   });
+
+  
+
+
+  myMapTemp.geoObjects.add(myPlacemarkTemp); // помещаем флажок на карту
+ 
+  // Получаем первый экземпляр коллекции слоев, потом первый слой коллекции
+  var layer = myMapTemp.layers.get(0).get(0);
+ 
+  // Решение по callback-у для определения полной загрузки карты
+  waitForTilesLoad(layer).then(function() {
+    // Скрываем индикатор загрузки после полной загрузки карты
+    spinner.removeClass('is-active');
+  });
+}
+ 
+// Функция для определения полной загрузки карты (на самом деле проверяется загрузка тайлов) 
+function waitForTilesLoad(layer) {
+  return new ymaps.vow.Promise(function (resolve, reject) {
+    var tc = getTileContainer(layer), readyAll = true;
+    tc.tiles.each(function (tile, number) {
+      if (!tile.isReady()) {
+        readyAll = false;
+      }
+    });
+    if (readyAll) {
+      resolve();
+    } else {
+      tc.events.once("ready", function() {
+        resolve();
+      });
+    }
+  });
+}
+ 
+function getTileContainer(layer) {
+  for (var k in layer) {
+    if (layer.hasOwnProperty(k)) {
+      if (
+        layer[k] instanceof ymaps.layer.tileContainer.CanvasContainer
+        || layer[k] instanceof ymaps.layer.tileContainer.DomContainer
+      ) {
+        return layer[k];
+      }
+    }
+  }
+  return null;
+}
+ 
+// Функция загрузки API Яндекс.Карт по требованию (в нашем случае при наведении)
+function loadScript(url, callback){
+  var script = document.createElement("script");
+ 
+  if (script.readyState){  // IE
+    script.onreadystatechange = function(){
+      if (script.readyState == "loaded" ||
+              script.readyState == "complete"){
+        script.onreadystatechange = null;
+        callback();
+      }
+    };
+  } else {  // Другие браузеры
+    script.onload = function(){
+      callback();
+    };
+  }
+ 
+  script.src = url;
+  document.getElementsByTagName("head")[0].appendChild(script);
+}
+ 
+// Основная функция, которая проверяет когда мы навели на блок с классом &#34;ymap-container&#34;
+var ymap = function() {
+  $('.ymap-container').mouseenter(function(){
+      if (!check_if_load) { // проверяем первый ли раз загружается Яндекс.Карта, если да, то загружаем
+ 
+	  	// Чтобы не было повторной загрузки карты, мы изменяем значение переменной
+        check_if_load = true; 
+ 
+		// Показываем индикатор загрузки до тех пор, пока карта не загрузится
+        spinner.addClass('is-active');
+ 
+		// Загружаем API Яндекс.Карт
+        loadScript("https://api-maps.yandex.ru/2.1/?lang=ru_RU&amp;loadByRequire=1", function(){
+           // Как только API Яндекс.Карт загрузились, сразу формируем карту и помещаем в блок с идентификатором &#34;map-yandex&#34;
+           ymaps.load(init);
+        });                
+      }
+    }
+  );  
+}
+ 
+$(function() {
+ 
+  //Запускаем основную функцию
+  ymap();
+ 
+});
 });
 
 
@@ -254,6 +348,25 @@ function backToTop() {
     e.preventDefault ();
     $('html').animate({scrollTop: 0}, 1000);
   });
+
+
+
+  var player;
+  $('.video__play').on('click', function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+      height: '460',
+      width: '100%',
+      videoId: 'Y8Q9ptCXzL0',
+      events: {
+        'onReady': videoPlay,
+      }
+    });
+  })
+
+  function videoPlay(event){
+    event.target.playVideo();
+  }
+
 
 }
 
